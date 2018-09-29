21e95f0da04fe185d935e7009227771d6eca6c2f
mbennett <mbennett5516@yahoo.com>
2018-09-29 11:48:13 -0500

    data pulling and populating

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,49 @@
+const name = $('#name');
+const ceo = $('#ceo');
+const price = $('#price');
+const chg = $('#chg');
+const chgPer = $('#chgPer');
+const high = $('#high');
+const low = $('#low');
+const logo = $('#logo');
+
+const getInfo = function(){
+
+    const stockSymbol = $('#symbol-input').val();
+    const queryURL = `https://api.iextrading.com/1.0/stock/${stockSymbol}/batch?types=quote,logo,news,company,chart&range=1d&last=5`;
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response){
+        console.log(response);
+        render(response);
+    })
+}
+
+const emptyAll = function(){
+    name.empty();
+    ceo.empty();
+    price.empty();
+    chg.empty();
+    chgPer.empty();
+    high.empty();
+    low.empty();
+    logo.empty();
+}
+
+const render = function(response){
+    emptyAll();
+    name.text(response.company.companyName)
+    ceo.text(response.company.CEO)
+    price.text(response.quote.latestPrice);
+    chg.text(response.quote.change);
+    chgPer.text(response.quote.changePercent)
+    high.text(response.quote.high);
+    low.text(response.quote.low);
+    logo.html(`<img src="${response.logo.url}"/>`);
+}
+
 $('#close-menu').on('click', function(){
     const menu = $('#side-menu');
     menu.css("width", "0");
@@ -22,3 +68,5 @@ $('.menu-item').on('click', function(){
     $('.menu-item').removeClass('active');
     $(this).addClass('active');
 })
+
+$('#submit').on('click', getInfo);
