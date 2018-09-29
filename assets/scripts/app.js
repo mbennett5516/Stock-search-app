c631b300585fca1d3afafe0dff91e8f90bfa9987
mbennett <mbennett5516@yahoo.com>
2018-09-29 09:27:25 -0500

    added favorites bar

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,4 +6,19 @@ $('#close-menu').on('click', function(){
 $('#open-menu').on('click', function(){
     const menu = $('#side-menu');
     menu.css("width", "100%");
+});
+
+$('#close-favs').on('click', function(){
+    const favs = $('#favs-menu');
+    favs.css("width", "0");
+})
+
+$('#open-favs').on('click', function(){
+    const favs = $('#favs-menu');
+    favs.css("width", "100%");
+});
+
+$('.menu-item').on('click', function(){
+    $('.menu-item').removeClass('active');
+    $(this).addClass('active');
 })
